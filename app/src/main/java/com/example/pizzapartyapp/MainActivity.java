6916cf0dee0c28e2a4a0a6fc2179298a7a8adbca
REECE <br0962806@otc.edu>
2023-02-15 10:12:33 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pizzapartyapp/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.pizzapartyapp;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static int SLICES_PER_PIZZA = 8;
+
+    private EditText mNumAttendEditText;
+    private TextView mNumPizzasTextView;
+    private RadioGroup mHowHungryRadioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Assign the widgets to fields
+        mNumAttendEditText = findViewById(R.id.num_attend_edit_text);
+        mNumPizzasTextView = findViewById(R.id.num_pizzas_text_view);
+        mHowHungryRadioGroup = findViewById(R.id.hungry_radio_group);
+
+        mNumAttendEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Toast.makeText(MainActivity.this, "TYPE BEN",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void calculateClick(View view) {
+
+        // Get the text that was typed into the EditText
+        String numAttendStr = mNumAttendEditText.getText().toString();
+
+        // Convert the text into an integer
+        int numAttend = Integer.parseInt(numAttendStr);
+
+        // Determine how many slices on average each person will eat
+        int slicesPerPerson = 0;
+        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+        if (checkedId == 0) {
+            slicesPerPerson = 2;
+        }
+        else if (checkedId == 1) {
+            slicesPerPerson = 3;
+        }
+        else if (checkedId == 2) {
+            slicesPerPerson = 4;
+        }
+
+        // Calculate and show the number of pizzas needed
+        int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson / (double) SLICES_PER_PIZZA);
+        mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
+    }
+}
